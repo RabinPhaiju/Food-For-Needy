75e77e4ac34c3887705f51092597e725cc5b0db3
Rabin Phaiju <rabinphaiju15@gmail.com>
2020-01-25 09:24:04 +0545

    login index edited

---
+++ html/js/index1.js
@@ -0,0 +1,25 @@
+let ParentMenus = document.querySelectorAll(".sidemenu>ul>li");
+ParentMenus.forEach(function(value, key) {
+    // ul>li>a
+    if (value.classList.contains('dropdown') && value.classList.contains('active')) {
+        value.classList.add("dropdown-open");
+        value.children[1].style.height = value.children[1].scrollHeight + "px";
+    }
+    value.children[0].onclick = (event) => {
+        ParentMenus.forEach(function(value2, key2) {
+            if (value2.classList.contains('dropdown') && key2 != key) {
+                value2.classList.remove("dropdown-open");
+                value2.children[1].style.height = "0px";
+            }
+        });
+        if (value.classList.contains('dropdown')) {
+            if (value.classList.contains('dropdown-open')) {
+                value.classList.remove("dropdown-open");
+                value.children[1].style.height = "0px";
+            } else {
+                value.classList.add("dropdown-open");
+                value.children[1].style.height = value.children[1].scrollHeight + "px";
+            }
+        } else {}
+    }
+});
