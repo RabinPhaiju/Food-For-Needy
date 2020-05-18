3a13d2d5262ea44f6c23853d045240c71af827e4
Rabin Phaiju <50291462+RabinPhaiju@users.noreply.github.com>
2020-05-18 21:40:37 +0545

    Update ffn.sql

--- DataBase IMPORT THIS sql/ffn.sql
+++ DataBase IMPORT THIS sql/ffn.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: May 17, 2020 at 01:43 PM
+-- Generation Time: May 18, 2020 at 05:55 PM
 -- Server version: 10.4.11-MariaDB
 -- PHP Version: 7.4.4
 
